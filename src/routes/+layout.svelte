18ded51a744f05d6dce1396ad60b6aa412846b3a
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-05-24 13:09:47 +0800

    other: project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+	import "../app.css";
+	import { Toaster } from 'svelte-french-toast';
+
+	let toastNewDefault = {
+		// success: {
+		// 	position: "bottom-left",
+		// },
+		error: {
+			duration: 10000,	// 10 seconds
+			// position: "bottom-right",
+		},  
+	};
+</script>
+
+
+<Toaster toastOptions={toastNewDefault}/>
+
+<slot/>
